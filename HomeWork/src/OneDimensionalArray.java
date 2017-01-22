00626c11a847850c29d55580942c8f53fe8dda24
Tomas Smilgius <tsmilgius@gmail.com>
2017-01-22 14:55:06 +0200

    Java docs aded

--- HomeWork/src/OneDimensionalArray.java
+++ HomeWork/src/OneDimensionalArray.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 /**
  * Created by tomas on 2017-01-22.
- * Mani
+ * Creates int[] and manipulates it in several ways.
  */
 public class OneDimensionalArray {
-    final static String GREETING_MESSAGE = "Įveskite masyvo dydį";
-    final static String CREATED_MESSAGE = "Suformuotas toks masyvas:";
-    final static String MANIPULATION_MESSAGE = "Po manipuliacijų masyvas atvaizduojamas taip:";
-    public static Scanner Input = new Scanner(System.in);
+    private final static String GREETING_MESSAGE = "Įveskite masyvo dydį";
+    private final static String CREATED_MESSAGE = "Suformuotas toks masyvas:";
+    private final static String MANIPULATION_MESSAGE = "Po manipuliacijų masyvas atvaizduojamas taip:";
+    private static Scanner Input = new Scanner(System.in);
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(GREETING_MESSAGE);
@@ -32,7 +32,7 @@ public class OneDimensionalArray {
      * @param arraySize int
      * @return array of int
      */
-    public int[] createArray(int arraySize){
+    private int[] createArray(int arraySize){
         int[] arraym = new int[arraySize];
         for (int i = 0; i < arraySize; i++) {
             arraym[i] = i;
@@ -47,30 +47,27 @@ public class OneDimensionalArray {
      * Modifies int[] which is given as parameter: all values which are multiples of 3 are multiplied by 2.
      * Other values are not changed. Then method prints all values from the highest position using for loop and
      * step variable 2; to ensure all the values are printed out (including  position 0) method checks if the length
-     * of the given array is even. Depending on the outcome it prints 2 values at the time.
+     * of the given array is even. Depending on the outcome it prints 2 values at the time or 2 values per iteration
+     * plus [0] position outside the loop.
      * @param arrayForModification int[].
      */
 
-    public static void modifyArray(int[] arrayForModification) throws InterruptedException {
+    private static void modifyArray(int[] arrayForModification) throws InterruptedException {
         for (int i = 0;  i < arrayForModification.length; i++){
             if( arrayForModification[i] % 3 == 0) {
                 arrayForModification[i] *= 2;
             }
         }
-
-            if (arrayForModification.length % 2 == 0) {
-                for(int j = arrayForModification.length - 1; j > 0; j -= 2 ) {
-                    System.out.println(arrayForModification[j]);
-                    System.out.println(arrayForModification[j - 1]);
-                }
-            } else {
-                for (int j = arrayForModification.length - 1; j > 0; j -= 2) {
-                    System.out.println(arrayForModification[j]);
-                    System.out.println(arrayForModification[j - 1]);
-                }
-                System.out.println(arrayForModification[0]);
-
+        if (arrayForModification.length % 2 == 0) {
+            for(int j = arrayForModification.length - 1; j > 0; j -= 2 ) {
+                System.out.println(arrayForModification[j]);
+                System.out.println(arrayForModification[j - 1]);
             }
-
+            for (int j = arrayForModification.length - 1; j > 0; j =- 2) {
+                System.out.println(arrayForModification[j]);
+                System.out.println(arrayForModification[j - 1]);
+            }
+            System.out.println(arrayForModification[0]);
+        }
     }
 }
--- HomeWork/src/TheGame.java
+++ HomeWork/src/TheGame.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 
 /**
  * Created by tomas on 2017-01-22.
- * Simple number guessing game. User gues
+ * Simple number guessing game. User guess number between 1 and 10. The game continues until number is guessed, or
+ * user selects 0 as an answer. If gusee is incorrect new number is generated and game continues. Program terminates
+ * if user's guess is higher than 10 or not a number. After guessingcorrect number, the programm shows how many tries
+ * it took to guess the right number.
  */
 
 
 public class TheGame {
 
-    final static String WELCOME_MESSAGE = "Labas, pabandyk atspėti skaičių nuo 1 iki 10. Jei nori pabaigti spausk 0";
-    final static String GUESSED_MESSAGE = " Atpėjai! ";
-    final static String MISSED_MESSAGE = "Neatspėjai,";
-    final static String GOODBYE_MESSAGE = "Viso gero! ";
-    final static String ONE_MORE_TIME = " bandom iš naujo";
-    final static String GENERATED_MESSAGE = " mano sugalvotas skaičius buvo ";
-    final static String ERROR_MESSAGE = "Galima įvesti tik skaičius nuo 1 iki 10 arba 0 išėjimui";
+    private final static String WELCOME_MESSAGE = "Labas, pabandyk atspėti skaičių nuo 1 iki 10. Jei nori pabaigti spausk 0";
+    private final static String GUESSED_MESSAGE = " Atpėjai! ";
+    private final static String MISSED_MESSAGE = "Neatspėjai,";
+    private final static String GOODBYE_MESSAGE = "Viso gero! ";
+    private final static String ONE_MORE_TIME = " bandom iš naujo";
+    private final static String GENERATED_MESSAGE = " mano sugalvotas skaičius buvo ";
+    private final static String ERROR_MESSAGE = "Galima įvesti tik skaičius nuo 1 iki 10 arba 0 išėjimui";
     private static int generated;
     private static  TheGame Game = new TheGame();
     private static Scanner Input = new Scanner(System.in);
@@ -44,19 +47,19 @@ public class TheGame {
     /**
      * generates a random number between 1 and 10 and returs it
      */
-    public  int generateNumber(){
+    private  int generateNumber(){
         Random rand = new Random();
         return rand.nextInt(10) + 1;
     }
 
     /**
      * Checks the answer and generates message according the outcome. It also changes gameState variable to false if
-     * win/exit conditions are met. If input is higher than 11 it terminates the program and
-     * @param guess
-     * @return
+     * win/exit conditions are met. If input is higher than 11 it terminates the program shows ERROR_MESSAGE.
+     * @param guess and genereated - integers;
+     *
      */
 
-    public void checkIfGuessed(int guess, int generated){
+    private void checkIfGuessed(int guess, int generated){
         if(guess == generated){
             System.out.println(GUESSED_MESSAGE + GOODBYE_MESSAGE);
             gameState = false;
