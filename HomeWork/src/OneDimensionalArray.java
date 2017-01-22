d9b502394b99892d5d159e8a8714560b934933f5
Tomas Smilgius <tsmilgius@gmail.com>
2017-01-22 14:31:21 +0200

    TheGame Class added

--- HomeWork/src/OneDimensionalArray.java
+++ HomeWork/src/OneDimensionalArray.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+
+
 /**
  * Created by tomas on 2017-01-22.
+ * Mani
  */
 public class OneDimensionalArray {
     final static String GREETING_MESSAGE = "Įveskite masyvo dydį";
@@ -10,10 +13,7 @@ public class OneDimensionalArray {
     final static String MANIPULATION_MESSAGE = "Po manipuliacijų masyvas atvaizduojamas taip:";
     public static Scanner Input = new Scanner(System.in);
 
-
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(GREETING_MESSAGE);
         int arraySize = Input.nextInt();
         OneDimensionalArray Array = new OneDimensionalArray();
@@ -23,13 +23,12 @@ public class OneDimensionalArray {
         System.out.println(Arrays.toString(originalArray));
         System.out.println(MANIPULATION_MESSAGE);
         modifyArray(originalArray);
-
     }
 
     /**
      * Creates one dimensional array of length 'arraySize', and fills with values coressponding
      * to the position number e.g arraySize[0] = 0, arraySize[1] = 1. If value is odd it becomes
-     * negative. Returns array.
+     * negative. Returns int[].
      * @param arraySize int
      * @return array of int
      */
@@ -45,19 +44,33 @@ public class OneDimensionalArray {
     }
 
     /**
-     * Modifies
-     * @param arrayForModification
+     * Modifies int[] which is given as parameter: all values which are multiples of 3 are multiplied by 2.
+     * Other values are not changed. Then method prints all values from the highest position using for loop and
+     * step variable 2; to ensure all the values are printed out (including  position 0) method checks if the length
+     * of the given array is even. Depending on the outcome it prints 2 values at the time.
+     * @param arrayForModification int[].
      */
 
-    public static void modifyArray(int[] arrayForModification){
+    public static void modifyArray(int[] arrayForModification) throws InterruptedException {
         for (int i = 0;  i < arrayForModification.length; i++){
             if( arrayForModification[i] % 3 == 0) {
                 arrayForModification[i] *= 2;
             }
         }
-        for(int j = arrayForModification.length - 1; j > 0; j -= 2 ){
-            System.out.println(arrayForModification[j]);
-            System.out.println(arrayForModification[j-1]);
-        }
+
+            if (arrayForModification.length % 2 == 0) {
+                for(int j = arrayForModification.length - 1; j > 0; j -= 2 ) {
+                    System.out.println(arrayForModification[j]);
+                    System.out.println(arrayForModification[j - 1]);
+                }
+            } else {
+                for (int j = arrayForModification.length - 1; j > 0; j -= 2) {
+                    System.out.println(arrayForModification[j]);
+                    System.out.println(arrayForModification[j - 1]);
+                }
+                System.out.println(arrayForModification[0]);
+
+            }
+
     }
 }
--- HomeWork/src/TheGame.java
+++ HomeWork/src/TheGame.java
@@ -1,8 +1,77 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by tomas on 2017-01-22.
+ * Simple number guessing game. User gues
  */
-public class TheGame {
-    public static void main(String[] args) {
 
+
+public class TheGame {
+
+    final static String WELCOME_MESSAGE = "Labas, pabandyk atspėti skaičių nuo 1 iki 10. Jei nori pabaigti spausk 0";
+    final static String GUESSED_MESSAGE = " Atpėjai! ";
+    final static String MISSED_MESSAGE = "Neatspėjai,";
+    final static String GOODBYE_MESSAGE = "Viso gero! ";
+    final static String ONE_MORE_TIME = " bandom iš naujo";
+    final static String GENERATED_MESSAGE = " mano sugalvotas skaičius buvo ";
+    final static String ERROR_MESSAGE = "Galima įvesti tik skaičius nuo 1 iki 10 arba 0 išėjimui";
+    private static int generated;
+    private static  TheGame Game = new TheGame();
+    private static Scanner Input = new Scanner(System.in);
+    private static int guess;
+    private static boolean gameState = true;
+
+
+    public static void main(String[] args) {
+        System.out.println(WELCOME_MESSAGE);
+        int counter = 0;
+        while(gameState) {
+            try {
+                generated = Game.generateNumber();
+                counter++;
+                guess = Input.nextInt();
+                Game.checkIfGuessed(guess, generated);
+
+            } catch (Exception e){
+                System.out.println(ERROR_MESSAGE);
+                System.exit(1);
+            }
+        }
+        System.out.println("Tau prireikė " + counter + " kartų, kad atspėtum mano sugalvotą skaičių");
+    }
+
+    /**
+     * generates a random number between 1 and 10 and returs it
+     */
+    public  int generateNumber(){
+        Random rand = new Random();
+        return rand.nextInt(10) + 1;
+    }
+
+    /**
+     * Checks the answer and generates message according the outcome. It also changes gameState variable to false if
+     * win/exit conditions are met. If input is higher than 11 it terminates the program and
+     * @param guess
+     * @return
+     */
+
+    public void checkIfGuessed(int guess, int generated){
+        if(guess == generated){
+            System.out.println(GUESSED_MESSAGE + GOODBYE_MESSAGE);
+            gameState = false;
+        }
+        else if (guess == 0){
+            System.out.println(GOODBYE_MESSAGE + GENERATED_MESSAGE + generated );
+            gameState = false;
+        }
+        else if (guess > 10){
+            System.out.println(ERROR_MESSAGE);
+            System.exit(1);
+        }
+        else{
+            System.out.println(MISSED_MESSAGE + GENERATED_MESSAGE + generated + ONE_MORE_TIME);
+            gameState = true;
+        }
     }
 }
